864b6efda15322ecc72b9b528d78a6be4adf730b
alfonsorhmx <37764364+alfonsorhmx@users.noreply.github.com>
2024-10-22 14:44:39 -0600

    updated

--- src/main/java/com/mycompany/holamundo/HolaMundo.java
+++ src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.holamundo;
 
-/**
- *
- * @author apple
- */
+
 public class HolaMundo {
 
     public static void main(String[] args) {
